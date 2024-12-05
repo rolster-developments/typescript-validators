b8741e1993dd4badfc25c210a6b658a193680717
ing.dacastillop <ing.dacastillop@gmail.com>
2024-12-05 10:39:59 -0500

    :sparkles: New validator 'defined' in Project

--- src/helpers.ts
+++ src/helpers.ts
@@ -8,79 +8,90 @@ import {
   regPassword
 } from './expressions';
 import validatorsI18n from './i18n';
-import { ValidatorFn } from './types';
+import { ValidatorFn, ValidatorResult, ValueState } from './types';
 
-export const required: ValidatorFn<any> = (value) => {
+export function defined<T = any>(value?: ValueState<T>): ValidatorResult {
+  return !!value
+    ? undefined
+    : {
+        id: 'defined',
+        message: validatorsI18n('defined')
+      };
+}
+
+export function required<T = any>(value?: ValueState<T>): ValidatorResult {
   return value
     ? undefined
     : {
         id: 'required',
         message: validatorsI18n('required')
       };
-};
+}
 
-export const textonly: ValidatorFn<string> = (value) => {
+export function textonly(value?: ValueState<string>): ValidatorResult {
   return value && !regOnlyText.test(value)
     ? {
         id: 'textonly',
         message: validatorsI18n('textonly')
       }
     : undefined;
-};
+}
 
-export const alphabetic: ValidatorFn<string> = (value) => {
+export function alphabetic(value?: ValueState<string>): ValidatorResult {
   return value && !regAlphabetic.test(value)
     ? {
         id: 'alphabetic',
         message: validatorsI18n('alphabetic')
       }
     : undefined;
-};
+}
 
-export const alphanumber: ValidatorFn<string> = (value) => {
+export function alphanumber(value?: ValueState<string>): ValidatorResult {
   return value && !regAlphanumber.test(value)
     ? {
         id: 'alphanumber',
         message: validatorsI18n('alphanumber')
       }
     : undefined;
-};
+}
 
-export const onlyNumber: ValidatorFn<string | number> = (value) => {
+export function onlyNumber(
+  value?: ValueState<string | number>
+): ValidatorResult {
   return value && !regOnlyNumber.test(String(value))
     ? {
         id: 'onlyNumber',
         message: validatorsI18n('onlynumber')
       }
     : undefined;
-};
+}
 
-export const decimal: ValidatorFn<number> = (value) => {
+export function decimal(value?: ValueState<number>): ValidatorResult {
   return value && !regDecimal.test(String(value))
     ? {
         id: 'decimal',
         message: validatorsI18n('decimal')
       }
     : undefined;
-};
+}
 
-export const email: ValidatorFn<string> = (value) => {
+export function email(value?: ValueState<string>): ValidatorResult {
   return value && !regEmail.test(value)
     ? {
         id: 'email',
         message: validatorsI18n('email')
       }
     : undefined;
-};
+}
 
-export const password: ValidatorFn<string> = (value) => {
+export function password(value?: ValueState<string>): ValidatorResult {
   return value && !regPassword.test(value)
     ? {
         id: 'password',
         message: validatorsI18n('password')
       }
     : undefined;
-};
+}
 
 export const strReqlength = (length: number): ValidatorFn<string> => {
   return (value) => {
--- src/i18n.ts
+++ src/i18n.ts
@@ -2,34 +2,36 @@ import { i18n } from '@rolster/i18n';
 
 const validatorsI18n = i18n({
   es: {
-    required: 'Campo es requerido',
-    textonly: 'Campo solo permite caracteres (sin espacio)',
     alphabetic: 'Campo solo permite caracteres',
     alphanumber: 'Campo solo permite caracteres y número',
-    onlynumber: 'Campo debe ser númerico',
     decimal: 'Campo debe ser número decimal',
+    defined: 'Campo debe estar definido',
     email: 'Campo debe ser correo electrónico',
-    minValue: 'Campo debe tener un valor mínimo de {value}',
     maxValue: 'Campo debe tener un valor máximo de {value}',
+    minValue: 'Campo debe tener un valor mínimo de {value}',
+    onlynumber: 'Campo debe ser númerico',
     password: 'Campo no permitido para password',
     reqlength: 'Campo debe tener {length} caracter(es)',
+    required: 'Campo es requerido',
     strMinlength: 'Campo debe tener mínimo {length} caracter(es)',
-    strMaxlength: 'Campo debe tener máximo {length} caracter(es)'
+    strMaxlength: 'Campo debe tener máximo {length} caracter(es)',
+    textonly: 'Campo solo permite caracteres (sin espacio)'
   },
   en: {
-    required: 'Field is required',
-    textonly: 'Field only allows characters (no space)',
     alphabetic: 'Field only allows characters',
     alphanumber: 'Field only allows characters and number',
-    onlynumber: 'Field must be numeric',
     decimal: 'Field must be decimal number',
+    defined: 'Field must be defined',
     email: 'Field must be email',
     minValue: 'Field must have a minimum value of {value}',
     maxValue: 'Field must have a maximum value of {value}',
+    onlynumber: 'Field must be numeric',
     password: 'Field not allowed for password',
     reqlength: 'Field must be {length} characters',
+    required: 'Field is required',
     strMinlength: 'Field must have minimum {length} characters',
-    strMaxlength: 'Field must have maximum {length} characters'
+    strMaxlength: 'Field must have maximum {length} characters',
+    textonly: 'Field only allows characters (no space)'
   }
 });
 
