45b27b182af4e4e0df8095863b3c95edea4043e6
ing.dacastillop <ing.dacastillop@gmail.com>
2025-09-08 11:18:06 -0500

    :lock: Remove dependencies with critical vulneravilities in Project

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,6 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import rolster from '@rolster/rollup';
 
-const plugins = [
-  commonjs(),
-  resolve(),
-  typescript({
-    tsconfig: './tsconfig.app.json',
-    declaration: true,
-    declarationDir: 'dist',
-    include: ['node_modules/@rolster/types/index.d.ts']
-  })
-];
-
-const rollupTs = (file) => {
-  return {
-    input: [`dist/esm/${file}.js`],
-    output: [
-      {
-        file: `dist/cjs/${file}.js`,
-        format: 'cjs',
-        sourcemap: true,
-        inlineDynamicImports: true
-      },
-      {
-        file: `dist/es/${file}.js`,
-        format: 'es',
-        sourcemap: true,
-        inlineDynamicImports: true
-      }
-    ],
-    external: ['@rolster/i18n'],
-    plugins
-  };
-};
-
-const exports = ['index', 'expressions', 'helpers'];
-
-export default [...exports.map((file) => rollupTs(file))];
+export default rolster({
+  entryFiles: ['index', 'expressions', 'helpers'],
+  packages: ['@rolster/i18n']
+});
